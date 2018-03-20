6483e0a7d632b7a8004faf7f21b5b4b0d68ec3d3
Nick <sonterixo@gmail.com>
2018-03-20 19:38:09 +0200

    favicon&logo

--- header.php
+++ header.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="shortcut icon" href="<? bloginfo('template_url') ?>/img/logo.png" type="image/x-icon">
-
     <? wp_head() ?>
 </head>
 <body>
